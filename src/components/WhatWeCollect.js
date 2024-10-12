b45987b54e0d9b9306a0e69c453837775980c9b6
Janaki Pal <153231016+JanakiPal@users.noreply.github.com>
2024-10-13 00:09:46 +0530

    wwc

---
+++ src/components/WhatWeCollect.js
@@ -0,0 +1,113 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import {BrowserRouter, Link} from "react-router-dom";
+import baloon from "../assets/images/wwc/baloon.jpg";
+import brokenPhone from "../assets/images/wwc/brokenPhone.jpg";
+import polybag1 from "../assets/images/wwc/polybag1.jpg";
+import wornShirt from "../assets/images/wwc/wornShirt.jpg";
+import WwcPlus from "../assets/images/wwc/wwcPlus.jpg";
+import "./WhatWeCollect.css";
+
+const WhatWeCollect = () => {
+	const data = [
+		{
+			id: 1,
+			route: "SoftPlastic",
+			heading: "SOFT PLASTIC",
+			text: "Flexible plastic than can be easily scrunched in your hand. ",
+			Eg: "E.g. bread bags, produce bags or chip packets.",
+			img: polybag1,
+		},
+		{
+			id: 2,
+			route: "ClothesTextiles",
+			heading: "CLOTHES AND TEXTILES",
+			text: "Clothes, shoes, accessories and linen in any condition.",
+			Eg: "",
+			img: wornShirt,
+		},
+		{
+			id: 3,
+			route: "EWaste",
+			heading: "E-WASTE",
+			text: "Small items that use electricity with a plug or batteries.  ",
+			Eg: "E.g. mobile phone, laptop or kettle.",
+			img: brokenPhone,
+		},
+		{
+			id: 4,
+			route: "Misfits",
+			heading: "MISFITS",
+			text: "Misfit items are other household items.",
+			Eg: "E.g. polystyrene, batteries, coffee cups or lightbulbs.",
+			img: baloon,
+		},
+	];
+	return (
+		<div className="wwc-container">
+			<div className="wwc-header">
+				{/* <img
+					src={WwcImage}
+					width="100%"
+					height={350}
+					className="wwc-img"
+				/> */}
+				<h1 className="header-text">WHAT WE COLLECT</h1>
+			</div>
+			<div className="middle">
+				<div className="wwc-info">
+					<h2>Meet the Everyday items</h2>
+					<p>
+						You can put these everyday household items all together
+						in any shopping bag you have at home.
+					</p>
+				</div>
+				<div className="wwc-items-wrap">
+					{data.map((item) => (
+						<div className="wwc-item" key={item.id}>
+							<div className="item-img-wrap">
+								<img src={item.img} />
+							</div>
+							<div className="item-info">
+								<h3>{item.heading}</h3>
+								<p>
+									{item.text}
+									<br />
+									<br />
+									{item.Eg}
+								</p>
+								<BrowserRouter>
+									<Link to={`/${item.route}`}>
+										Find Out More
+									</Link>
+								</BrowserRouter>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<hr />
+			<div className="plus">
+				<div className="plus-wrap">
+					<div className="plus-img-wrap">
+						<img src={WwcPlus} />
+					</div>
+					<div className="plus-text">
+						<h1>PLUS+ Items</h1>
+						<p>
+							We also collect tricky-to-recycle items that show up
+							occasionally.
+							<br />
+							E.g. clunky cardboard or big electronics.
+						</p>
+						<BrowserRouter>
+							<Link to="/Plus">Find Out More</Link>
+						</BrowserRouter>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default WhatWeCollect;
